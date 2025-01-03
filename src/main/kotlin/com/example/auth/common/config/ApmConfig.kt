205363ba2bd94491d6d3c8aa9cb69a49c7b9ec30
PARKPARKWOO <wy9295@naver.com>
2025-01-03 14:57:48 +0900

    feat: Enable pyroscope configuration in spring context

--- src/main/kotlin/com/example/auth/common/config/ApmConfig.kt
+++ src/main/kotlin/com/example/auth/common/config/ApmConfig.kt
@@ -1,7 +1,9 @@
 package com.example.auth.common.config
 
+import org.springframework.context.annotation.Configuration
 import org.woo.apm.pyroscope.EnablePyroscope
 
 @EnablePyroscope
+@Configuration
 class ApmConfig {
 }
