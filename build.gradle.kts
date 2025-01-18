69285ae12c11ad0e7ee4aeb713338b3d072e4c78
PARKPARKWOO <wy9295@naver.com>
2025-01-19 03:20:09 +0900

    feat: Update grpc module version

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
     }
 
     // grpc
-    implementation("org.woo:grpc:0.0.7-SNAPSHOT")
+    implementation("org.woo:grpc:0.1.2")
     implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
     // https://mvnrepository.com/artifact/net.devh/grpc-spring-boot-starter
 //    implementation("net.devh:grpc-spring-boot-starter:3.1.0.RELEASE")
