6396de12be5602105f8cca38a261a644072ea746
PARKPARKWOO <wy9295@naver.com>
2025-04-14 19:36:51 +0900

    feat: Add id annotation in application-authority, application-domain

--- src/main/kotlin/com/example/auth/domain/entity/application/ApplicationAuthority.kt
+++ src/main/kotlin/com/example/auth/domain/entity/application/ApplicationAuthority.kt
@@ -1,10 +1,12 @@
 package com.example.auth.domain.entity.application
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("application_authority")
 class ApplicationAuthority(
+    @Id
     @Column("id")
     val id: Long = 0L,
     @Column("application_id")
--- src/main/kotlin/com/example/auth/domain/entity/application/ApplicationDomain.kt
+++ src/main/kotlin/com/example/auth/domain/entity/application/ApplicationDomain.kt
@@ -1,10 +1,12 @@
 package com.example.auth.domain.entity.application
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("application_domain")
 class ApplicationDomain(
+    @Id
     @Column("id")
     val id: Long = 0L,
     @Column("application_id")
