283fc56865e90576fa8e065b070e7db573228f7e
PARKPARKWOO <wy9295@naver.com>
2025-07-21 21:16:01 +0900

    feat: Add idempotent key prefix

--- src/main/kotlin/com/example/auth/presentation/grpc/TokenGrpcController.kt
+++ src/main/kotlin/com/example/auth/presentation/grpc/TokenGrpcController.kt
@@ -4,12 +4,10 @@ import com.example.auth.business.service.JwtTokenService
 import com.example.auth.domain.repository.redis.RedisDriver
 import dto.JwtResponseDto
 import io.grpc.Status
-import io.grpc.StatusException
 import net.devh.boot.grpc.server.service.GrpcService
 import org.woo.apm.log.log
 import org.woo.auth.grpc.TokenProto
 import org.woo.auth.grpc.TokenServiceGrpcKt
-import kotlin.math.log
 
 @GrpcService
 class TokenGrpcController(
@@ -20,23 +18,24 @@ class TokenGrpcController(
         const val REISSUE_TOKEN_LOCK_WAIT_TIME = 50L
         const val REISSUE_TOKEN_LOCK_LEASE_TIME = 3000L
         const val TOKEN_CACHE_TTL = 300L // 5 min
+        const val ROTATION_IDEMPOTENT_KEY_PREFIX = "rotation:idempotent"
     }
 
     override suspend fun reissueToken(request: TokenProto.ReissueTokenRequest): TokenProto.JwtTokenResponse {
         log().info("incoming reissue-token")
         // TODO: refreshToken 예외처리 필요함
-        val refreshTokenString = jwtTokenService.getUserIdFromRefreshToken(request.refreshToken).toString()
-        return redisDriver.useLockOrNull(refreshTokenString, REISSUE_TOKEN_LOCK_WAIT_TIME, REISSUE_TOKEN_LOCK_LEASE_TIME) {
+        val idempotentKey = ROTATION_IDEMPOTENT_KEY_PREFIX + jwtTokenService.getUserIdFromRefreshToken(request.refreshToken).toString()
+        return redisDriver.useLockOrNull(idempotentKey, REISSUE_TOKEN_LOCK_WAIT_TIME, REISSUE_TOKEN_LOCK_LEASE_TIME) {
             log().info("use lock")
-            redisDriver.getValue(refreshTokenString, JwtResponseDto::class.java)?.let {
+            redisDriver.getValue(idempotentKey, JwtResponseDto::class.java)?.let {
                 log().info("getValue")
                 return@useLockOrNull it.toProto()
             }
             jwtTokenService.rotationToken(request.refreshToken).also {
                 log().info("rotationToken")
-                redisDriver.setValue(refreshTokenString, it, TOKEN_CACHE_TTL)
+                redisDriver.setValue(idempotentKey, it, TOKEN_CACHE_TTL)
             }.toProto()
-        } ?: redisDriver.getValue(refreshTokenString, JwtResponseDto::class.java)?.toProto()
+        } ?: redisDriver.getValue(idempotentKey, JwtResponseDto::class.java)?.toProto()
         ?: throw Status.UNAVAILABLE.withDescription("Failed to acquire retry lock").asRuntimeException()
     }
 
